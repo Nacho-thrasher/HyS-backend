3f8aa54352f785ed772348fc93a0093cecaf15a3
Auzchwitz <nachoburgos1995@gmail.com>
2021-08-03 12:30:43 -0300

    spa control

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 
 const express = require('express');
 const { dbConnection } = require('./database/config');
@@ -31,7 +32,10 @@ app.use('/api/login', require('./routes/auth'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/upload', require('./routes/uploads'));
 
-
+// lo ultimo 
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+})
 
 app.listen(process.env.PORT, ()=>{
     console.log('servidor corriendo en puerto'+ process.env.PORT)
