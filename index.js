9347893e2205a6d6363ef7b51ba8e684904b615b
Auzchwitz <nachoburgos1995@gmail.com>
2021-10-01 12:00:19 -0300

    preload skeleton

--- index.js
+++ index.js
@@ -54,7 +54,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public/index.html'));
 })
 
-
 app.listen(process.env.PORT, ()=>{
     console.log('servidor corriendo en puerto'+ process.env.PORT)
 });
